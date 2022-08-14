98bbab3249e8d38689629b08bbcc1a089fb8dbef
hieptn102 <102493682+hieptn102@users.noreply.github.com>
2022-08-15 00:29:32 +0700

    1
    1

---
+++ bt5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bt5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

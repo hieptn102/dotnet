70d0bbba806ef00a6860813a7a95987ecbbe8973
Hiep Nguyen T.[2] VN.Hanoi <106299716+mvn-hiepnguyen2-hn@users.noreply.github.com>
2022-08-15 17:11:45 +0700

    2
    dotnet

--- bt5/Program.cs
+++ bt5/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-
+using System.Text.RegularExpressions;
 namespace bt5
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Nhập vào MSSV: ");
+            string sinput = Console.ReadLine();
+            if (Regex.IsMatch(sinput,"B\\d{7}"))
+                Console.WriteLine("MSV hợp lệ");
+            else
+                Console.WriteLine("MSV không hợp lệ");
+         }
     }
 }
